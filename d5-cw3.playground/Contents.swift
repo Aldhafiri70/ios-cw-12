fc1878ff4694062319dd4066f28254836c0efe3e
Aldhafiri70 <thelordshoes@Gmail.com>
2022-03-16 20:30:54 +0300

    done

--- d5-cw3.playground/Contents.swift
+++ d5-cw3.playground/Contents.swift
@@ -1,3 +1,40 @@
 import UIKit
+import Darwin
+print("Abdulrazaq")
 
-var greeting = "Hello, playground"
+func grade(frist:Int,Second:Int,Final:Int)->String{
+    let grade = frist + Second + Final
+    if (grade >= 90&&grade <= 100){
+        return "A"
+    
+    }
+       
+    
+
+else if(grade >= 80&&grade <= 89){
+    return "B"
+}
+else if(grade >= 70&&grade <= 79){
+    return "C"
+}
+else if(grade >= 60&&grade <= 69){
+    return "D"
+}
+else{
+    return "F"
+}
+}
+grade(frist: 90, Second: 80, Final: 70)
+
+func nums(num:Int, num2:Int){
+ print(num+num2)
+ print(num-num2)
+print(num*num2)
+print(num/num2)
+    
+    
+    
+    
+}
+nums(num: 20, num2: 10)
+
